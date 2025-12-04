2b3154ed8a23c6f73f3db5554d052a143a1985bd
Drew Hayward <drew.hayward@shopify.com>
2025-12-04 12:54:16 -0500

    day 3 pt 2

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,4 +1,4 @@
-use std::fmt::format;
+use std::{cmp::max, collections::HashMap};
 
 use crate::solution::Solution;
 
@@ -13,34 +13,78 @@ fn parse_banks(puzzle_input: String) -> Vec<Vec<i64>> {
         .collect()
 }
 
+/// concat adjacent digits
+fn combine(digits: &[i64]) -> i64 {
+    let mut acc = 0;
+    for d in digits {
+        acc = acc * 10 + d;
+    }
+
+    acc
+}
+
+fn max_substring(digits: &[i64], length: usize) -> i64 {
+    let mut memo: HashMap<(usize, usize), i64> = HashMap::new();
+    // Build mem table for ascending sizes
+    for l in 1..=length {
+        for pos in (0..digits.len()).rev() {
+            if pos + l > digits.len() {
+                continue;
+            }
+
+            // base case
+            if pos + l == digits.len() {
+                let val = combine(&digits[pos..pos + l]);
+                memo.insert((pos, l), val);
+                continue;
+            }
+
+            // what is the max substring starting at pos of length l?
+            // we don't use the current digit and lookup the best result
+            // from the suffix
+            let max_suffix: i64 = *memo
+                .get(&(pos + 1, l))
+                .expect("should have just visited this cell");
+
+            // We use this digit and the suffix of l-1
+            let use_digit: i64 = match l {
+                1 => digits[pos],
+                _ => {
+                    let max_short_suffix = *memo
+                        .get(&(pos + 1, l - 1))
+                        .expect("should have visited this shorter cell");
+                    let num_digits = (max_short_suffix as f64).log10().floor() as u32 + 1;
+                    digits[pos] * 10_i64.pow(num_digits) + max_short_suffix
+                }
+            };
+
+            let val = max(max_suffix, use_digit);
+            memo.insert((pos, l), val);
+        }
+    }
+
+    *memo
+        .get(&(0, length))
+        .expect("should have populated on this cell")
+}
+
 pub struct Lobby;
 
 impl Solution for Lobby {
     fn part1(puzzle_input: String) -> String {
-        let banks = parse_banks(puzzle_input);
-
-        let mut total: i64 = 0;
-        for bank in banks {
-            let mut max = 0;
-
-            for (i, d1) in bank.iter().enumerate() {
-                if i == bank.len() {
-                    break
-                }
-
-                for d2 in &bank[i+1..] {
-                    max = max.max(d1*10+d2);
-                }
-            }
-
-            total += max;
-        }
-
-        total.to_string()
+        parse_banks(puzzle_input)
+            .iter()
+            .map(|b| max_substring(b, 2))
+            .sum::<i64>()
+            .to_string()
     }
 
     fn part2(puzzle_input: String) -> String {
-        todo!()
+        parse_banks(puzzle_input)
+            .iter()
+            .map(|b| max_substring(b, 12))
+            .sum::<i64>()
+            .to_string()
     }
 }
 
@@ -60,6 +104,11 @@ mod test {
 
     #[test]
     fn test_part_2() {
-        assert_eq!(Lobby::part2(TEST_INPUT.to_owned()), "");
+        assert_eq!(Lobby::part2(TEST_INPUT.to_owned()), "3121910778619");
+    }
+
+    #[test]
+    fn test_simple() {
+        assert_eq!(max_substring(&vec![9, 1, 2], 2), 92);
     }
 }
